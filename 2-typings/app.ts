21b4de1268d408ef093316744b28f7c41595f202
Kirill <redbull09010@gmail.com>
2025-10-20 11:06:05 +0300

    DZ

--- 2-typings/app.ts
+++ 2-typings/app.ts
@@ -1,38 +1,27 @@
-'use strict';
+const enum Numbers {
+    TEN = 10,
+    ONE_HUNDRED = 100,
+    ONE_THOUSAND = 1000,
+    ONE_MILLION = 1000000,
+    ONE_BILLION = 1000000000,
+    ONE_TRILLION = 1000000000000,
+    ONE_QUADRILLION = 1000000000000000,
+    MAX = 9007199254740992
+}
 
-var makeOrdinal = require('./makeOrdinal');
-var isFinite = require('./isFinite');
-var isSafeNumber = require('./isSafeNumber');
-
-var TEN = 10;
-var ONE_HUNDRED = 100;
-var ONE_THOUSAND = 1000;
-var ONE_MILLION = 1000000;
-var ONE_BILLION = 1000000000;           //         1.000.000.000 (9)
-var ONE_TRILLION = 1000000000000;       //     1.000.000.000.000 (12)
-var ONE_QUADRILLION = 1000000000000000; // 1.000.000.000.000.000 (15)
-var MAX = 9007199254740992;             // 9.007.199.254.740.992 (15)
-
-var LESS_THAN_TWENTY = [
+const LESS_THAN_TWENTY :string[] = [
     'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
     'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
 ];
 
-var TENTHS_LESS_THAN_HUNDRED = [
+const TENTHS_LESS_THAN_HUNDRED :string[] = [
     'zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
 ];
 
-/**
- * Converts an integer into words.
- * If number is decimal, the decimals will be removed.
- * @example toWords(12) => 'twelve'
- * @param {number|string} number
- * @param {boolean} [asOrdinal] - Deprecated, use toWordsOrdinal() instead!
- * @returns {string}
- */
-function toWords(number, asOrdinal) {
-    var words;
-    var num = parseInt(number, 10);
+
+function toWords(number:number|string, asOrdinal:boolean):string {
+    let words:string;
+    let num:number = parseInt(number.toString(), 10);
 
     if (!isFinite(num)) {
         throw new TypeError(
@@ -48,12 +37,12 @@ function toWords(number, asOrdinal) {
     return asOrdinal ? makeOrdinal(words) : words;
 }
 
-function generateWords(number) {
-    var remainder, word,
-        words = arguments[1];
+function generateWords(number:number):string {
+    let remainder:number=0, word:string='',
+        words:string[] = arguments[1];
 
     // We’re done
-    if (number `===` 0) {
+    if (number === 0) {
         return !words ? 'zero' : words.join(' ').replace(/,$/, '');
     }
     // First run
@@ -70,43 +59,41 @@ function generateWords(number) {
         remainder = 0;
         word = LESS_THAN_TWENTY[number];
 
-    } else if (number < ONE_HUNDRED) {
-        remainder = number % TEN;
-        word = TENTHS_LESS_THAN_HUNDRED[Math.floor(number / TEN)];
+    } else if (number < Numbers.ONE_HUNDRED) {
+        remainder = number % Numbers.TEN;
+        word = TENTHS_LESS_THAN_HUNDRED[Math.floor(number / Numbers.TEN)];
         // In case of remainder, we need to handle it here to be able to add the “-”
         if (remainder) {
             word += '-' + LESS_THAN_TWENTY[remainder];
             remainder = 0;
         }
 
-    } else if (number < ONE_THOUSAND) {
-        remainder = number % ONE_HUNDRED;
-        word = generateWords(Math.floor(number / ONE_HUNDRED)) + ' hundred';
+    } else if (number < Numbers.ONE_THOUSAND) {
+        remainder = number % Numbers.ONE_HUNDRED;
+        word = generateWords(Math.floor(number / Numbers.ONE_HUNDRED)) + ' hundred';
 
-    } else if (number < ONE_MILLION) {
-        remainder = number % ONE_THOUSAND;
-        word = generateWords(Math.floor(number / ONE_THOUSAND)) + ' thousand,';
+    } else if (number < Numbers.ONE_MILLION) {
+        remainder = number % Numbers.ONE_THOUSAND;
+        word = generateWords(Math.floor(number / Numbers.ONE_THOUSAND)) + ' thousand,';
 
-    } else if (number < ONE_BILLION) {
-        remainder = number % ONE_MILLION;
-        word = generateWords(Math.floor(number / ONE_MILLION)) + ' million,';
+    } else if (number < Numbers.ONE_BILLION) {
+        remainder = number % Numbers.ONE_MILLION;
+        word = generateWords(Math.floor(number / Numbers.ONE_MILLION)) + ' million,';
 
-    } else if (number < ONE_TRILLION) {
-        remainder = number % ONE_BILLION;
-        word = generateWords(Math.floor(number / ONE_BILLION)) + ' billion,';
+    } else if (number < Numbers.ONE_TRILLION) {
+        remainder = number % Numbers.ONE_BILLION;
+        word = generateWords(Math.floor(number / Numbers.ONE_BILLION)) + ' billion,';
 
-    } else if (number < ONE_QUADRILLION) {
-        remainder = number % ONE_TRILLION;
-        word = generateWords(Math.floor(number / ONE_TRILLION)) + ' trillion,';
+    } else if (number < Numbers.ONE_QUADRILLION) {
+        remainder = number % Numbers.ONE_TRILLION;
+        word = generateWords(Math.floor(number / Numbers.ONE_TRILLION)) + ' trillion,';
 
-    } else if (number <= MAX) {
-        remainder = number % ONE_QUADRILLION;
-        word = generateWords(Math.floor(number / ONE_QUADRILLION)) +
+    } else if (number <= Numbers.MAX) {
+        remainder = number % Numbers.ONE_QUADRILLION;
+        word = generateWords(Math.floor(number / Numbers.ONE_QUADRILLION)) +
         ' quadrillion,';
     }
 
     words.push(word);
     return generateWords(remainder, words);
 }
-
-module.exports = toWords;
